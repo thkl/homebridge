de005c583afe38689caea3e83c2673399a9b781a
Thomas Kluge <th.kluge@me.com>
2015-10-24 10:37:04 +0200

    some changes to make it run with the latest homebridge version

--- platforms/HomeMaticPlatform.js
+++ platforms/HomeMaticPlatform.js
@@ -1,4 +1,4 @@
-var types = require("HAP-NodeJS/accessories/types.js");
+var types = require("hap-nodejs/accessories/types.js");
 var xmlrpc = require('homematic-xmlrpc')
 
 var request = require("request");
--- platforms/homematic/HomematicChannel.js
+++ platforms/homematic/HomematicChannel.js
@@ -1,4 +1,4 @@
-var types = require("HAP-NodeJS/accessories/types.js");
+var types = require("hap-nodejs/accessories/types.js");
 
 
 function HomeMaticGenericChannel(log,platform, id ,name, type ,adress,special) {

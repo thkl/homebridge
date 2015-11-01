ec7f76b19db1c5cd7adcddac155d6e5d5cece3e3
Thomas Kluge <th.kluge@me.com>
2015-11-01 10:55:21 +0100

    implemented Homematic Smoke Detectors

--- platforms/HomematicChannel.js
+++ platforms/HomematicChannel.js
@@ -544,6 +544,31 @@ HomeMaticGenericChannel.prototype = {
 	 });
 	}
 	
+	// Smoke Detector
+	if (this.type=="SMOKE_DETECTOR") { 
+	 cTypes.push(
+	 {  
+	 	cType: "00000076-0000-1000-8000-0026BB765291",
+         
+        onRead: function(callback) {
+            that.query("STATE",callback);
+        },
+        
+        onRegister: function(characteristic) { 
+            that.currentStateCharacteristic["STATE"] = characteristic;
+            characteristic.eventEnabled = true;
+            that.remoteGetValue("STATE");
+        },
+      
+      perms: ["pr","ev"],
+      format: "bool",
+      initialValue: that.dpvalue("STATE",0),
+      supportEvents: false,
+      supportBonjour: false,
+      manfDescription: "Smoke detected"
+	 });
+	}
+	
 	// Heating Device
 	
 	if ((this.type=="CLIMATECONTROL_RT_TRANSCEIVER") || (this.type=="THERMALCONTROL_TRANSMIT")) {
@@ -659,6 +684,9 @@ HomeMaticGenericChannel.prototype = {
 	  return types.LOCK_MECHANISM_STYPE
 	}
 	
+	if (this.type=="SMOKE_DETECTOR") {
+	  return "00000087-0000-1000-8000-0026BB765291";
+	}
 	
 	
   },
